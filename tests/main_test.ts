1fc5017ca9976d1f7a4dfcd609b1a0531a6e73f7
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2025-02-13 13:31:28 +0100

    Creating a shortcode for url

--- tests/main_test.ts
+++ tests/main_test.ts
@@ -1,6 +1,34 @@
-import { assertEquals } from "@std/assert";
-import { add } from "../src/main.ts";
+import { assertEquals, assertNotEquals, assertRejects } from "@std/assert";
+import { delay } from "jsr:@std/async/delay";
+import { generateShortCode } from "../src/db.ts";
 
-Deno.test(function addTest() {
-	assertEquals(add(2, 3), 5);
+const TestUrl = {
+	valid:
+		"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map",
+	invalid: "/docs/Web/JavaScript/Reference/Global_Obj",
+};
+Deno.test("URL Shortener", async (t) => {
+	await t.step("Should generate a short code for a valid URL", async () => {
+		const longUrl = TestUrl.valid;
+		const shortCode = await generateShortCode(longUrl);
+
+		assertEquals(typeof shortCode, "string");
+		assertEquals(shortCode.length, 11);
+	});
+	await t.step("should be unique for each timestamp", async () => {
+		const longUrl = TestUrl.valid;
+		const a = await generateShortCode(longUrl);
+		await delay(5);
+		const b = await generateShortCode(longUrl);
+
+		assertNotEquals(a, b);
+	});
+
+	await t.step("throw error on bad URL", () => {
+		const longUrl = TestUrl.invalid;
+
+		assertRejects(async () => {
+			await generateShortCode(longUrl);
+		});
+	});
 });
