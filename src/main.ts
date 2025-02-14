8f9c9970375594323d9a2d01ffda0f850146a553
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2025-02-14 12:03:16 +0100

    Added get request

--- src/main.ts
+++ src/main.ts
@@ -6,36 +6,35 @@ const app = new Router();
 app.get("/", () => new Response("Hello World"));
 
 app.post("/links", async (req) => {
-    try {
-        const { longUrl } = await req.json();
-        const shortCode = await generateShortCode(longUrl);
-        const res = await storeShortLink(longUrl,shortCode,"testUser")
-        
-        
-        return new Response(JSON.stringify(res), {
-            status: 201,    
-          });
-        
-    } catch (error) {
-        throw new Error(`An error occurred: ${error}`)
-    }
+	try {
+		const { longUrl } = await req.json();
+		const shortCode = await generateShortCode(longUrl);
+		const res = await storeShortLink(longUrl, shortCode, "testUser");
+
+		return new Response(JSON.stringify(res), {
+			status: 201,
+		});
+	} catch (error) {
+		throw new Error(`An error occurred: ${error}`);
+	}
 });
 
 app.get("/links/:id", async (_req, _info, params) => {
-
-    const shortCode = params;
-    console.log(shortCode)
-    // const data = await getShortLink(shortCode!)
-  
-    return new Response(JSON.stringify(shortCode), {
-      status: 200,
-      headers: {
-        "content-type": "application/json",
-      },
-  
-    });
-  
-  })
+	const shortCode = _info?.pathname.groups.id;
+	if (!shortCode) throw new Error("The value wasn't provided");
+	try {
+		// biome-ignore lint/style/noNonNullAssertion: <explanation>
+		const data = await getShortLink(shortCode as string);
+		return new Response(JSON.stringify(data), {
+			status: 200,
+			headers: {
+				"content-type": "application/json",
+			},
+		});
+	} catch (error) {
+		throw new Error(`An error occurred: ${error}`);
+	}
+});
 
 export default {
 	fetch(req) {
